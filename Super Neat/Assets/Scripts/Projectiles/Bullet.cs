c88153ab7e4aaec444c47f624dac94bca07feec5
Chrums <jchrumka@chrumka.local>
2016-02-27 23:41:09 -0500

    calling it a night

--- Super Neat/Assets/Scripts/Projectiles/Bullet.cs
+++ Super Neat/Assets/Scripts/Projectiles/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : Projectile {
 		Character character = other.GetComponent<Character>();
 		if(character != null) {
 			character.Kill();
+			Destroy(gameObject);
 		}
 
 		Projectile projectile = other.GetComponent<Projectile>();
